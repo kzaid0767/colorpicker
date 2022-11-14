82196620575b23e54978cf2db8704c89d44ae70f
Kassim Zaid <81573513+kzaid0767@users.noreply.github.com>
2022-11-14 14:05:01 -0500

    schemes are working need to finish styling

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const form = document.querySelector('#input-form')
+const outputDiv = document.querySelector('#output')
 
 form.addEventListener('submit', handleSubmit)
+
+//let colorArr = []
+function rendeColorDivs(arr){
+    let html=``
+    
+    for (color of arr){
+        html += `
+        <div><div class="eachColor" style="background-color: ${color};"></div><div>${color}</div></div>`
+    }
+    
+    outputDiv.innerHTML = html
+}
 
 function handleSubmit(e){
     e.preventDefault()
     const formInfo = new FormData(form)
-    const color = formInfo.get('color-pick')
+    let color = formInfo.get('color-pick')
+    color = color.slice(1,color.length)
     schemeMode = formInfo.get('scheme-select')
-    fetch(`https://www.thecolorapi.com/scheme?hex=0047AB`)
+    fetch(`https://www.thecolorapi.com/scheme?hex=${color}&mode=${schemeMode}&count=6`)
         .then(res=>res.json())
-        .then(data=>console.log(data ))
+        .then(data=>{
+            let colorArr = []
+            for(let obj of data.colors){
+                colorArr.push(obj.hex.value)
+            }
+            rendeColorDivs(colorArr )
+    })
 }
-
